784a867bed6d048028be2dbc305f1d71e91de55b
PauloHenriqueCarvalho <paulohenrque7carvalho@gmail.com>
2024-02-29 17:30:33 -0300

    Tela Login e Poke Responsive

---
+++ js/login.js
@@ -0,0 +1,35 @@
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");  
+var Indicator =  document.getElementById("Indicator");  
+
+function register(){
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(60px)";
+}
+
+function login(){
+    RegForm.style.transform = "translateX(340px)";
+    LoginForm.style.transform = "translateX(340px)";
+    Indicator.style.transform = "translateX(-60px)";
+}
+
+function verificacao(){
+    let user= document.getElementById("usuario").value;
+    let password= document.getElementById("senha").value;
+
+
+    
+
+    if(user.trim() === "" || password.trim() === ""  ){
+        alert("Preencha todos os campos");
+    } else {
+        if(user.trim() === "adm" || password.trim() === "adm") {
+            window.location.href = ("../pages/poke-page.html");
+        }
+        else{
+            alert("Usuario ou senha incorreto!");
+        }
+        
+    }
+}
